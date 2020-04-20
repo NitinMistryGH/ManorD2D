1305b3802ee33d1f29dc10cae8018fe584dba88b
Monika-Mistry <monika.mistry1@gmail.com>
2020-04-20 15:14:08 +0100

    Banner Component Added

--- manor-d2d-ui/src/App.js
+++ manor-d2d-ui/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {CssBaseline} from '@material-ui/core'
-import Footer from './components/Footer/Footer';
-import {ThemeProvider} from '@material-ui/core/styles'
+
+import {CssBaseline} from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/core/styles';
+
 import theme from './theme/theme';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
 
 
-function App() {
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NavBar />
+      <Banner currentPage={{title: "Database of Streets Visited", link: "/", breadcrumb: "Sankirtana Forever"}}/>
       <Footer />
     </ ThemeProvider>
   );
--- manor-d2d-ui/src/components/Banner/Banner.jsx
+++ manor-d2d-ui/src/components/Banner/Banner.jsx
@@ -1,25 +1,65 @@
-import React from 'react';
+import React from "react";
 import {
-    AppBar,
-    Toolbar
-} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+  AppBar,
+  Toolbar,
+  Typography,
+  Breadcrumbs,
+  Link,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
-const  useStyles = makeStyles(theme => ({
-
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: red[800],
+    padding: "5% 7% 4.8% 7%",
+  },
+  title: {
+    letterSpacing: "1px",
+    wordWrap: "break-word",
+    fontSize: "2rem",
+    fontWeight: 500,
+  },
+  links: {
+    color: theme.palette.common.white,
+  },
 }));
 
-const Banner = () => {
+const Banner = ({ currentPage }) => {
   const styles = useStyles();
-  return(
-      <AppBar>
-          <Toolbar />
-      </AppBar>
-  )
 
-}
+  return (
+    <AppBar position="relative" className={styles.appBar}>
+      <Toolbar disableGutters>
+        <Grid container>
+          <Grid item md={9}>
+            <Typography variant="h1" className={styles.title}>
+              {currentPage.title}
+            </Typography>
+          </Grid>
+          <Grid item md={3}>
+            <Breadcrumbs aria-label="breadcrumb">
+              <Link href="/" className={styles.links} underline="none">
+                Home
+              </Link>
+              <Link
+                href={currentPage.link}
+                className={styles.links}
+                aria-current="page"
+                underline="none"
+              >
+                {currentPage.breadcrumb}
+              </Link>
+            </Breadcrumbs>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Banner;
 
-Bannner.defaultProps = {};
+Banner.defaultProps = {};
 Banner.propTypes = {};
