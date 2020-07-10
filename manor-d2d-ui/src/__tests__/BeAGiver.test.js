6f3fdf5f6779010e8bad07e693f093bb4cc77c2b
Monika-Mistry <monika.mistty@gmail.com>
2020-07-10 21:14:38 +0100

    finished unit test

--- manor-d2d-ui/src/__tests__/BeAGiver.test.js
+++ manor-d2d-ui/src/__tests__/BeAGiver.test.js
@@ -11,6 +11,11 @@ describe('Be A Giver Content renders correctly', () => {
     test('text content', () => {
         render(<BeAGiver  styles={styles}/>);
 
-        expect(document.getElementById('beagiver-content')).contains('This Prabhupada Marathon, join an international celebration of Giving - sign up for the Be A Giver Campaign!')
+        expect(document.getElementById('beagiver-content').textContent).toContain('This Prabhupada Marathon, join an international celebration of Giving - sign up for the Be A Giver Campaign!')
+    });
+    test('video', () => {
+        render(<BeAGiver  styles={styles}/>);
+
+        expect(document.getElementById('beagiver-video').title).toEqual("Be A Giver Prabhupada Marathon 2018")
     })
 })
