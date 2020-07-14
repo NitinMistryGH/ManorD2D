c36370bc976ee51bca0109b891a22320c7c898ad
Monika-Mistry <monika.mistty@gmail.com>
2020-07-14 16:52:43 +0100

    adding photo grid

--- manor-d2d-ui/src/components/Photo/Photo.jsx
+++ manor-d2d-ui/src/components/Photo/Photo.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {
+    Grid
+} from '@material-ui/core';
 
-import {PhotoList} from './PhotoList';
+import { PhotoList } from './PhotoList';
 import PhotoCard from './PhotoCard';
 
 const useStyles = makeStyles(theme => ({
-
+    photo: {
+        width: 150,
+        height: 150,
+        margin: '5%'
+    },
+    card: {
+        width: 'calc(150px + 10%)'
+    }
 }))
 
 
@@ -13,16 +23,20 @@ const Photo = ({ }) => {
     const styles = useStyles();
 
     return (
-        <div>
+        <Grid container spacing={7}>
             {
                 PhotoList.map(photo => (
-                    <PhotoCard
-                        id={`photo-${photo.title}`}
-                        key={`photo-${photo.title}`}
-                        photo={photo} />
+                    <Grid item >
+                        <PhotoCard
+                            id={`photo-${photo.title}`}
+                            key={`photo-${photo.title}`}
+                            photo={photo}
+                            styles={styles}
+                        />
+                    </Grid>
                 ))
             }
-        </div>
+        </Grid>
     )
 
 }
--- manor-d2d-ui/src/components/Photo/PhotoCard.jsx
+++ manor-d2d-ui/src/components/Photo/PhotoCard.jsx
@@ -4,14 +4,17 @@ import {
     CardMedia
 } from '@material-ui/core';
 
-const PhotoCard = ({ photo }) => (
-    <Card>
+const PhotoCard = ({ photo, styles }) => (
+    <Card className={styles.card} raised>
+        <a href={photo.image} target="_parent">
         <CardMedia
+            className={styles.photo}
             component="img"
             alt={photo.title}
             image={photo.image}
             title={photo.title}
         />
+        </a>
     </Card>
 )
 
